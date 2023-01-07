81e2510323d4a8ce57965eeb907c19d1e8b48d85
SewoongPark <49395754+Heron-Woong@users.noreply.github.com>
2023-01-07 13:44:02 +0900

    JPA_활용1

---
+++ java/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,32 @@
+package jpabook.jpashop.repository;
+
+import jakarta.persistence.EntityManager;
+import jpabook.jpashop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager em;
+
+    public void save(Item item){
+        if(item.getId() == null){ //완전히 새로운 값을 가져온다.
+            em.persist(item);
+        } else{ //원래 있던것에 업데이트 됨
+            em.merge(item);
+        }
+    }
+
+    public Item findOne(Long id){
+        return em.find(Item.class, id);
+    }
+
+    public List<Item> findAll(){
+        return em.createQuery("select i from Item i", Item.class)
+                .getResultList();
+    }
+}
